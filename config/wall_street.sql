fea6a52ff76c13a9713a71f9b6a8bfb1f201278f
AudreyBliss <audrey94300@hotmail.fr>
2018-11-05 16:35:33 +0100

    addArtist

--- config/wall_street.sql
+++ config/wall_street.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 30 oct. 2018 à 13:25
+-- Généré le :  lun. 05 nov. 2018 à 10:31
 -- Version du serveur :  5.7.21
 -- Version de PHP :  5.6.35
 
@@ -56,18 +56,18 @@ CREATE TABLE IF NOT EXISTS `artiste` (
   `photo` varchar(255) NOT NULL,
   `description` mediumtext NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `artiste`
 --
 
 INSERT INTO `artiste` (`id`, `nom`, `photo`, `description`) VALUES
-(1, 'Banksy', 'banksy.jpg', 'Quapropter a natura mihi videtur potius quam ab indigentia orta amicitia, applicatione magis animi cum quodam sensu amandi quam cogitatione quantum illa res utilitatis esset habitura. Quod quidem quale sit, etiam in bestiis quibusdam animadverti potest, quae ex se natos ita amant ad quoddam tempus et ab eis ita amantur ut facile earum sensus appareat. Quod in homine multo est evidentius, primum ex ea caritate quae est inter natos et parentes, quae dirimi nisi detestabili scelere non potest; deinde cum similis sensus exstitit amoris, si aliquem nacti sumus cuius cum moribus et natura congruamus, quod in eo quasi lumen aliquod probitatis et virtutis perspicere videamur.'),
-(2, 'Jef Aérosol', 'jefaerosol.jpg', 'Quapropter a natura mihi videtur potius quam ab indigentia orta amicitia, applicatione magis animi cum quodam sensu amandi quam cogitatione quantum illa res utilitatis esset habitura. Quod quidem quale sit, etiam in bestiis quibusdam animadverti potest, quae ex se natos ita amant ad quoddam tempus et ab eis ita amantur ut facile earum sensus appareat. Quod in homine multo est evidentius, primum ex ea caritate quae est inter natos et parentes, quae dirimi nisi detestabili scelere non potest; deinde cum similis sensus exstitit amoris, si aliquem nacti sumus cuius cum moribus et natura congruamus, quod in eo quasi lumen aliquod probitatis et virtutis perspicere videamur.'),
-(3, 'C 215', 'c215.jpg', 'Quapropter a natura mihi videtur potius quam ab indigentia orta amicitia, applicatione magis animi cum quodam sensu amandi quam cogitatione quantum illa res utilitatis esset habitura. Quod quidem quale sit, etiam in bestiis quibusdam animadverti potest, quae ex se natos ita amant ad quoddam tempus et ab eis ita amantur ut facile earum sensus appareat. Quod in homine multo est evidentius, primum ex ea caritate quae est inter natos et parentes, quae dirimi nisi detestabili scelere non potest; deinde cum similis sensus exstitit amoris, si aliquem nacti sumus cuius cum moribus et natura congruamus, quod in eo quasi lumen aliquod probitatis et virtutis perspicere videamur.'),
-(4, 'MiSS.Tic', 'misstic.jpg', 'Quapropter a natura mihi videtur potius quam ab indigentia orta amicitia, applicatione magis animi cum quodam sensu amandi quam cogitatione quantum illa res utilitatis esset habitura. Quod quidem quale sit, etiam in bestiis quibusdam animadverti potest, quae ex se natos ita amant ad quoddam tempus et ab eis ita amantur ut facile earum sensus appareat. Quod in homine multo est evidentius, primum ex ea caritate quae est inter natos et parentes, quae dirimi nisi detestabili scelere non potest; deinde cum similis sensus exstitit amoris, si aliquem nacti sumus cuius cum moribus et natura congruamus, quod in eo quasi lumen aliquod probitatis et virtutis perspicere videamur.'),
-(5, 'Blek Le Rat', 'bleklerat.jpg', 'Quapropter a natura mihi videtur potius quam ab indigentia orta amicitia, applicatione magis animi cum quodam sensu amandi quam cogitatione quantum illa res utilitatis esset habitura. Quod quidem quale sit, etiam in bestiis quibusdam animadverti potest, quae ex se natos ita amant ad quoddam tempus et ab eis ita amantur ut facile earum sensus appareat. Quod in homine multo est evidentius, primum ex ea caritate quae est inter natos et parentes, quae dirimi nisi detestabili scelere non potest; deinde cum similis sensus exstitit amoris, si aliquem nacti sumus cuius cum moribus et natura congruamus, quod in eo quasi lumen aliquod probitatis et virtutis perspicere videamur.');
+(1, 'Banksy', 'banksy.jpg', 'Banksy serait né à Bristol en Grande-Bretagne en 1974. Difficile de sortir du conditionnel car l’artiste a passé sa vie à échapper aux médias afin de garder l’anonymat.\r\nIndigné, irrévérencieux, révolutionnaire, il adore provoquer, choquer voire perturber la société et c\'est ce qui fait toute l\'importance de son oeuvre. \r\nPochoirs, peintures, sculptures, détournements d’objets urbains ou d’œuvres classiques,transposition d\'images,une partie peinte à l\'aérosol installations… Banksy est un artiste polyvalent.  \r\nhttp://banksy.co.uk/'),
+(2, 'Jef Aérosol', 'jefaerosol.jpg', 'Tantôt pochoiriste, photo-graphiste, peintre ou musicien, Jef Aérosol est un artiste aux multiples talents. Symbole de la nouvelle vague du street art français, il sème ses œuvres singulières signées d’une flèche rouge depuis le début des années 80.Son travail est  identifiable par la flèche rouge qu’il appose systématiquement comme signature. Fan des plasticiens Ernest Pignon-Ernestou du photographe Georges Rousse, Jef Aérosol s’essaie très tôt à la technique du photo-graphisme.  L’art de transformer les images à partir de photomatons, de photocopies, de découpages et de collages qui jouent avec l’ombre et la lumière. Ces fabrications maisons deviendront ses premiers pochoirs.\r\nhttps://www.jefaerosol.com/'),
+(3, 'C 215', 'c215.jpg', 'C215 alias Christian Guémy est un artiste urbain, pochoiriste français né en 1973.Il s’est d’abord initié au graffiti, qu’il a délaissé rapidement pour se consacrer au pochoir. Les œuvres de C215 sont principalement à tailles humaines pour être au plus proches du réel et des passants, il va s’en dégager des couleurs étincelantes et un éclairage particulièrement travaillé. On va percevoir le caractère de ses modèles à travers les traits et les expressions qui sont dépeints. Il peint des scènes de rue ainsi que des portraits de sans abris, mendiants, réfugiés, d’orphelins, toutes ces personnes laissées pour compte par la société capitaliste. \r\nhttps://www.c215.fr/C215/HOME.html'),
+(4, 'MiSS.Tic', 'misstic.jpg', 'Grande figure du Street Art, Miss.Tic a fait des rues de la capitale sa plus belle galerie. Depuis 1985, elle bombe au pochoir, sur les murs, des billets d\'humeur illustrés de portraits de femmes, légendés de phrases pertinentes et impertinentes. \r\nDans son atelier parisien, elle réalise ses pochoirs à l\'aide de matrices fabriquées à partir de photos agrandies et découpées au cutter et inspirées de photos tirées dans les magazines féminins Elle développe l\'image de la femme pour la promouvoir sans questionnement. C\'est de l\'art accessible à tous, personne n\'y résiste.\r\nhttp://missticinparis.com/'),
+(5, 'Blek Le Rat', 'bleklerat.jpg', 'Blek le rat est le pseudonyme de Xavier Prou,un des tout premiers artistes graffiti à Paris et le fondateur du mouvement du pochoir. Ses premiers pochoirs représentent des petits rats noirs qu’il fait courir le long des murs. Le rat, l’anagramme du mot ART, est pour Blek « le seul animal qui survivra l’apocalypse ». Ses peintures représentent généralement des figures anonymes ou célèbres, de taille réelle. Il puise souvent son inspiration dans des personnages du quotidien, issus de la réalité urbaine, dont il reproduit ensuite l\'image dans le décor où il évolue.\r\nhttp://blekleratoriginal.com/fr/');
 
 -- --------------------------------------------------------
 
@@ -81,7 +81,6 @@ CREATE TABLE IF NOT EXISTS `oeuvre` (
   `titre` varchar(30) NOT NULL,
   `photo` varchar(255) NOT NULL,
   `localisation` varchar(250) NOT NULL,
-  `date` year(4) NOT NULL,
   `artiste_id` tinyint(2) UNSIGNED DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `foreign_key` (`artiste_id`)
@@ -91,37 +90,37 @@ CREATE TABLE IF NOT EXISTS `oeuvre` (
 -- Déchargement des données de la table `oeuvre`
 --
 
-INSERT INTO `oeuvre` (`id`, `titre`, `photo`, `localisation`, `date`, `artiste_id`) VALUES
-(1, 'Dreams', 'bdreams.jpg', 'Boston / Massachusetts / U.S.A', 2010, 1),
-(2, 'Minnie', 'bminnie.jpg', 'Paris / France', 2018, 1),
-(3, 'Mother Caring', 'bmothercaring.jpg', 'New York / U.S.A', 2007, 1),
-(4, 'Out of bed', 'boutofbed.jpg', 'Melrose et Orange Avenue / Los Angeles / U.S.A', 2008, 1),
-(5, 'Steve Jobs', 'bstevejobs.jpg', 'Calais / Hauts-de-France / France', 2015, 1),
-(6, 'The girl', 'bthegirl.png', 'Bataclan / Paris / France', 2018, 1),
-(7, 'Basquiat', 'jbasquiat.jpg', 'Brooklyn / New York / U.S.A', 2010, 2),
-(8, 'Chuuut', 'jchuuut.jpg', 'Quartier Beauboug / Paris / France', 2011, 2),
-(9, 'John Lennon', 'jjohnlennon.jpg', 'Quartier Mouffetard / Paris / France', 2010, 2),
-(10, 'Nelson Mandela', 'jnelsonmandela.jpg', 'Caen / Normandie / France', 2011, 2),
-(11, 'Octobre rose', 'joctobrerose.jpg', 'C.H.U / Bordeaux / France', 2013, 2),
-(12, 'Sitting Kid', 'jsittingkid.jpg', 'Quartier des Buttes Chaumont / Paris', 2013, 2),
-(13, 'Angel', 'blangel.jpg', 'Dubai / Emirats Arabes Unis', 2015, 5),
-(14, 'Blek that\'s better', 'blblekthatsbetter.png', 'Londres / Angleterre', 2012, 5),
-(15, 'Homeless', 'blhomeless.jpg', 'Canal St Martin / Paris / France', 2013, 5),
-(16, 'Joseph Beuys', 'bljosephbeuys.png', 'Paris / France', 1991, 5),
-(17, 'Runnig Man in Berlin', 'blrunningmaninberlin.jpg', 'Berlin / Allemagne', 2010, 5),
-(18, 'Thug of war', 'blthugofwar.jpg', 'Oregon / U.S.A', 2018, 5),
-(19, 'Bird girl', 'cbirdgirl.jpg', 'Paris 13e / France', 2008, 3),
-(20, 'Dali', 'cdali.jpg', 'Madrid / Espagne', 2015, 3),
-(21, 'Le chat', 'clechat.jpg', 'Djerba / Tunisie', 2012, 3),
-(22, 'Lincoln', 'clincoln.jpg', 'Vincennes / France', 2016, 3),
-(23, 'Moon girl', 'cmoongirl.jpg', 'Londres / Angleterre', 2014, 3),
-(24, 'Vieil homme', 'cvieilhomme.png', 'Vitry / France', 2017, 3),
-(25, 'Abus', 'mabus.jpg', 'Paris / France', 2016, 4),
-(26, 'Art Rock', 'martrock.jpg', 'Butte aux cailles / Paris 13e / France', 2013, 4),
-(27, 'Ca va passer', 'mcavapasser.jpg', 'Paris / France', 1990, 4),
-(28, 'Illusion', 'millusion.jpg', 'Paris 13e / France', 2010, 4),
-(29, 'Le Vain', 'mlevain.jpg', 'Paris / France', 1995, 4),
-(30, 'Simple', 'msimple.jpg', 'Paris / France', 1985, 4);
+INSERT INTO `oeuvre` (`id`, `titre`, `photo`, `localisation`, `artiste_id`) VALUES
+(1, 'Dreams', 'bdreams.jpg', 'Boston / Massachusetts / U.S.A', 1),
+(2, 'Minnie', 'bminnie.jpg', 'Paris / France', 1),
+(3, 'Mother Caring', 'bmothercaring.jpg', 'New York / U.S.A', 1),
+(4, 'Out of bed', 'boutofbed.jpg', 'Melrose et Orange Avenue / Los Angeles / U.S.A', 1),
+(5, 'Steve Jobs', 'bstevejobs.jpg', 'Calais / Hauts-de-France / France', 1),
+(6, 'The girl', 'bthegirl.png', 'Bataclan / Paris / France', 1),
+(7, 'Basquiat', 'jbasquiat.jpg', 'Brooklyn / New York / U.S.A', 2),
+(8, 'Chuuut', 'jchuuut.jpg', 'Quartier Beauboug / Paris / France', 2),
+(9, 'John Lennon', 'jjohnlennon.jpg', 'Quartier Mouffetard / Paris / France', 2),
+(10, 'Nelson Mandela', 'jnelsonmandela.jpg', 'Caen / Normandie / France', 2),
+(11, 'Octobre rose', 'joctobrerose.jpg', 'C.H.U / Bordeaux / France', 2),
+(12, 'Sitting Kid', 'jsittingkid.jpg', 'Quartier des Buttes Chaumont / Paris', 2),
+(13, 'Angel', 'blangel.jpg', 'Dubai / Emirats Arabes Unis', 5),
+(14, 'Blek that\'s better', 'blblekthatsbetter.png', 'Londres / Angleterre', 5),
+(15, 'Homeless', 'blhomeless.jpg', 'Canal St Martin / Paris / France', 5),
+(16, 'Joseph Beuys', 'bljosephbeuys.png', 'Paris / France', 5),
+(17, 'Runnig Man in Berlin', 'blrunningmaninberlin.jpg', 'Berlin / Allemagne', 5),
+(18, 'Thug of war', 'blthugofwar.jpg', 'Oregon / U.S.A', 5),
+(19, 'Bird girl', 'cbirdgirl.jpg', 'Paris 13e / France', 3),
+(20, 'Dali', 'cdali.jpg', 'Madrid / Espagne', 3),
+(21, 'Le chat', 'clechat.jpg', 'Djerba / Tunisie', 3),
+(22, 'Lincoln', 'clincoln.jpg', 'Vincennes / France', 3),
+(23, 'Moon girl', 'cmoongirl.jpg', 'Londres / Angleterre', 3),
+(24, 'Vieil homme', 'cvieilhomme.png', 'Vitry / France', 3),
+(25, 'Abus', 'mabus.jpg', 'Paris / France', 4),
+(26, 'Art Rock', 'martrock.jpg', 'Butte aux cailles / Paris 13e / France', 4),
+(27, 'Ca va passer', 'mcavapasser.jpg', 'Paris / France', 4),
+(28, 'Illusion', 'millusion.jpg', 'Paris 13e / France', 4),
+(29, 'Le Vain', 'mlevain.jpg', 'Paris / France', 4),
+(30, 'Simple', 'msimple.jpg', 'Paris / France', 4);
 
 --
 -- Contraintes pour les tables déchargées
--- controllers/addArtist.php
+++ controllers/addArtist.php
@@ -3,59 +3,55 @@
 require_once '../config/database.php';
 
 $admin = null;
+$message = null;
+$isEmpty = null;
 
 
-function checkInput($data){
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
+if(!empty($_POST['nom']) AND !empty($_POST['description']) AND isset($_POST['nom'], $_POST['description'])){     /* !empty + isset */ 
+$nom = $_POST['nom'];
+$description = $_POST['description'];     
+
+$file_name = $_FILES['photo']['name'];  /*  pour comprendre la variable $_FILES >>> http://php.net/manual/fr/features.file-upload.post-method.php     */
+$file_type = $_FILES['photo']['type'];
+$file_tmp_name = $_FILES['photo']['tmp_name'];
+$file_extension = strrchr($file_name, ".");         /* prend ce qu'il y a après le point, l'extension */
+$file_destination = '../public/img/artist/'.$file_name;        /* chemin.nom du fichier*/
+
+$valid_extensions = array('.jpg', '.JPG', '.png', '.PNG', '.jpeg', '.JPEG');    //C'est mieux de vérifier le format de ton image, tu peux ajouter des extensions
+
+$imageArtist = $pdo->prepare('SELECT * FROM artiste WHERE photo = ?');   
+$imageArtist->execute(array($file_destination));
+$verifArtist = $imageArtist->rowCount();   /* Compte dans ta colonne photo si le chemin apparait, s'il apparait = 1 , sinon, 0 */
+                                            /* Ca évite que ton enregistrement se répète plusieurs fois */
 
 
-$nomArtiste = checkInput($_POST['nom']);
-$description = checkInput($_POST['description']);
-
-if(!empty($nomArtiste) AND !empty($description) AND isset($nomArtiste, $description)){  /* !empty + isset */      
+    if($verifArtist == 0){	/* n'apparait pas donc le fichier peut être uploader */
+        if(in_array($file_extension, $valid_extensions)){
     
-     
-    
-    $file_name = $_FILES['photo']['name'];  /*  pour comprendre la variable $_FILES >>> http://php.net/manual/fr/features.file-upload.post-method.php     */
-    $file_type = $_FILES['photo']['type'];
-    $file_tmp_name = $_FILES['photo']['tmp_name'];
-    $file_extension = strrchr($file_name, ".");         /* prend ce qu'il y a après le point, l'extension */
-    $file_destination = ''.$file_name;        /* chemin.nom du fichier*/
+            if(move_uploaded_file($file_tmp_name, $file_destination)){  /* déplace fichier de son emplacement temporaire vers ta bdd */
+                
+                $req = $pdo->prepare("INSERT INTO artiste (nom, photo, description) VALUES(?, ?, ?)");		
+                $req->execute(array($nom, $file_name, $description));
+                    $message = "Téléchargement terminé";
 
-    $valid_extensions = array('.jpg', '.JPG', '.png', '.PNG', '.jpeg', '.JPEG');
+                    header('location:showArtist.php');
     
-    //$photo = $pdo->prepare('SELECT * FROM artiste WHERE photo = ?');   
-   // $photo->execute(array($file_destination));
-    //$verifPhoto = $photo->rowCount();   /* Compte dans ta colonne photo si le chemin apparait, s'il apparait = 1 , sinon, 0 */
-                                        /* Ca évite que ton enregistrement se répète plusieurs fois */
-        if($verifPhoto == 0){	/* n'apparait pas donc le fichier peut être uploader */
-            if(in_array($file_extension, $valid_extensions)){
-        
-                if(move_uploaded_file($file_tmp_name, $file_destination)){  /* déplace fichier de son emplacement temporaire vers ta bdd */
-                    
-                    $req = $pdo->prepare("INSERT INTO artiste (photo, description, nom) VALUES(?, ?, ?)");		
-                    $req->execute(array($file_destination, $description, $nomArtiste));
-                    header("location: showArtist.php");	
-        
-                }else{
-                    echo 'image n a pas été envoyé';
-                }
-        
             }else{
-        
-                echo 'Seuls les fichiers au format... sont autorisé'; //l'extension de l'image n'est pas comprise dans le tableau $valid_extensions
+                $message = 'L\'image n\'a pas pu se télécharger';
             }
-
-        }else{  /* donc = 1 */
-            echo 'photo déjà existante';
+    
+        }else{
+    
+            $message =  'Le format ne l\'image n\'est pas autorisé'; //l'extension de l'image n'est pas comprise dans le tableau $valid_extensions
         }
 
+    }else{  /* donc = 1 */
+        $message = 'Image déjà existante';
+    }
+
+
 }else{
-    echo 'files vide, tout le champs ne sont pas rempli';
+$isEmpty = 'Tous les champs doivent être complétés';
 }
 
 
