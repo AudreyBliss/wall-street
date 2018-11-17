076fa4e52f37de7962d5d2943bfc6536d1707e6c
AudreyBliss <audrey94300@hotmail.fr>
2018-11-17 12:35:33 +0100

    bdd

--- config/wall_street.sql
+++ config/wall_street.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 05 nov. 2018 à 10:31
+-- Généré le :  sam. 17 nov. 2018 à 11:33
 -- Version du serveur :  5.7.21
 -- Version de PHP :  5.6.35
 
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `admin` (
 --
 
 INSERT INTO `admin` (`id`, `email`, `password`) VALUES
-(2, 'admin', '$2y$10$y.x0AzafXw82Qw3LEXLMbOdpQMRyRlXP7j//P7rk17El8rtMBAVBi');
+(2, 'admin', '940c0f26fd5a30775bb1cbd1f6840398d39bb813');
 
 -- --------------------------------------------------------
 
@@ -56,18 +56,18 @@ CREATE TABLE IF NOT EXISTS `artiste` (
   `photo` varchar(255) NOT NULL,
   `description` mediumtext NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `artiste`
 --
 
 INSERT INTO `artiste` (`id`, `nom`, `photo`, `description`) VALUES
-(1, 'Banksy', 'banksy.jpg', 'Banksy serait né à Bristol en Grande-Bretagne en 1974. Difficile de sortir du conditionnel car l’artiste a passé sa vie à échapper aux médias afin de garder l’anonymat.\r\nIndigné, irrévérencieux, révolutionnaire, il adore provoquer, choquer voire perturber la société et c\'est ce qui fait toute l\'importance de son oeuvre. \r\nPochoirs, peintures, sculptures, détournements d’objets urbains ou d’œuvres classiques,transposition d\'images,une partie peinte à l\'aérosol installations… Banksy est un artiste polyvalent.  \r\nhttp://banksy.co.uk/'),
-(2, 'Jef Aérosol', 'jefaerosol.jpg', 'Tantôt pochoiriste, photo-graphiste, peintre ou musicien, Jef Aérosol est un artiste aux multiples talents. Symbole de la nouvelle vague du street art français, il sème ses œuvres singulières signées d’une flèche rouge depuis le début des années 80.Son travail est  identifiable par la flèche rouge qu’il appose systématiquement comme signature. Fan des plasticiens Ernest Pignon-Ernestou du photographe Georges Rousse, Jef Aérosol s’essaie très tôt à la technique du photo-graphisme.  L’art de transformer les images à partir de photomatons, de photocopies, de découpages et de collages qui jouent avec l’ombre et la lumière. Ces fabrications maisons deviendront ses premiers pochoirs.\r\nhttps://www.jefaerosol.com/'),
-(3, 'C 215', 'c215.jpg', 'C215 alias Christian Guémy est un artiste urbain, pochoiriste français né en 1973.Il s’est d’abord initié au graffiti, qu’il a délaissé rapidement pour se consacrer au pochoir. Les œuvres de C215 sont principalement à tailles humaines pour être au plus proches du réel et des passants, il va s’en dégager des couleurs étincelantes et un éclairage particulièrement travaillé. On va percevoir le caractère de ses modèles à travers les traits et les expressions qui sont dépeints. Il peint des scènes de rue ainsi que des portraits de sans abris, mendiants, réfugiés, d’orphelins, toutes ces personnes laissées pour compte par la société capitaliste. \r\nhttps://www.c215.fr/C215/HOME.html'),
-(4, 'MiSS.Tic', 'misstic.jpg', 'Grande figure du Street Art, Miss.Tic a fait des rues de la capitale sa plus belle galerie. Depuis 1985, elle bombe au pochoir, sur les murs, des billets d\'humeur illustrés de portraits de femmes, légendés de phrases pertinentes et impertinentes. \r\nDans son atelier parisien, elle réalise ses pochoirs à l\'aide de matrices fabriquées à partir de photos agrandies et découpées au cutter et inspirées de photos tirées dans les magazines féminins Elle développe l\'image de la femme pour la promouvoir sans questionnement. C\'est de l\'art accessible à tous, personne n\'y résiste.\r\nhttp://missticinparis.com/'),
-(5, 'Blek Le Rat', 'bleklerat.jpg', 'Blek le rat est le pseudonyme de Xavier Prou,un des tout premiers artistes graffiti à Paris et le fondateur du mouvement du pochoir. Ses premiers pochoirs représentent des petits rats noirs qu’il fait courir le long des murs. Le rat, l’anagramme du mot ART, est pour Blek « le seul animal qui survivra l’apocalypse ». Ses peintures représentent généralement des figures anonymes ou célèbres, de taille réelle. Il puise souvent son inspiration dans des personnages du quotidien, issus de la réalité urbaine, dont il reproduit ensuite l\'image dans le décor où il évolue.\r\nhttp://blekleratoriginal.com/fr/');
+(1, 'Banksy', 'banksy.jpg', 'Banksy serait né à Bristol en Grande-Bretagne en 1974. Difficile de sortir du conditionnel car l’artiste a passé sa vie à échapper aux médias afin de garder l’anonymat.\r\nIndigné, irrévérencieux, révolutionnaire, il adore provoquer, choquer voire perturber la société et c\'est ce qui fait toute l\'importance de son oeuvre.Enfant chéri du monde artistique de par le monde et cauchemar des autorités policières, son art est un mélange d\'ironie, d\'humour et comporte très souvent des messages très clairs, dans l\'optique où ils ne sont pas interprétés au premier degré. \r\nPochoirs, peintures, sculptures, détournements d’objets urbains ou d’œuvres classiques,transposition d\'images,une partie peinte à l\'aérosol installations… Banksy est un artiste polyvalent.  \r\n'),
+(2, 'Jef Aérosol', 'jefaerosol.jpg', 'Tantôt pochoiriste,peintre ou musicien, Jef Aérosol est un artiste aux multiples talents.\r\nEnfant du rock, il est biberonné aux Clash, Hendrix, Dylan et autres Rolling Stones dont il collectionne les posters. Ces affiches épinglées sur les murs de sa chambre sont ses premières sources d’inspiration. Son travail est  identifiable par la flèche rouge qu’il appose systématiquement comme signature. Fan des plasticiens Ernest Pignon-Ernestou du photographe Georges Rousse, Jef Aérosol s’essaie très tôt à la technique du photo-graphisme.  L’art de transformer les images à partir de photomatons, de photocopies, de découpages et de collages qui jouent avec l’ombre et la lumière. Ces fabrications maisons deviendront ses premiers pochoirs.\r\n'),
+(3, 'C 215', 'c215.jpg', 'C215 alias Christian Guémy est un artiste urbain, pochoiriste français né en 1973.Il s’est d’abord initié au graffiti, qu’il a délaissé rapidement pour se consacrer au pochoir.Passionné par la Renaissance, il se forge une culture humaniste.C215 puise ses influences au travers des siècles d’art.Armé de sa bombe et de son spray, C215 redonne aux murs gris et sales un nouveau souffle esthétique, dans des zones industrielles où le béton ne laisse jusqu’alors pas de place à l’émotion. Il les habille de portraits colorés, réalisés au pochoir ou à main levée. Les œuvres de C215 sont principalement à tailles humaines pour être au plus proches du réel et des passants, il va s’en dégager des couleurs étincelantes et un éclairage particulièrement travaillé. On va percevoir le caractère de ses modèles à travers les traits et les expressions qui sont dépeints. Il peint des scènes de rue ainsi que des portraits de sans abris, mendiants, réfugiés, d’orphelins, toutes ces personnes laissées pour compte par la société capitaliste. \r\n'),
+(4, 'MiSS.Tic', 'misstic.jpg', 'Grande figure du Street Art, Miss.Tic a fait des rues de la capitale sa plus belle galerie, elle est depuis trente ans la reine du street art à la française.Miss.Tic bombe au pochoir, sur les murs, des billets d\'humeur illustrés de portraits de femmes, légendés de phrases pertinentes et impertinentes. Son image fétiche est une belle brune sexy, très proche de son propre personnage, exposée dans des postures lascives mais jamais vulgaires. Elle s\'accompagne souvent d\'une phrase mi-philosophique mi-humoristique mais toujours poétique. Dans son atelier parisien, elle réalise ses pochoirs à l\'aide de matrices fabriquées à partir de photos agrandies et découpées au cutter et inspirées de photos tirées dans les magazines féminins Elle développe l\'image de la femme pour la promouvoir sans questionnement. C\'est de l\'art accessible à tous, personne n\'y résiste.\r\n'),
+(5, 'Blek Le Rat', 'bleklerat.jpg', 'Blek le rat est le pseudonyme de Xavier Prou,un des tout premiers artistes graffiti à Paris et le fondateur du mouvement du pochoir. Ses premiers pochoirs représentent des petits rats noirs qu’il fait courir le long des murs. Le rat, l’anagramme du mot ART, est pour Blek « le seul animal qui survivra l’apocalypse » un pseudo à la gloire d’un animal qui le fascine et qu’il commence à peindre dans tout Paris dans les années 80, histoire de rappeler aux parisiens que les rats sont partout dans leur ville.Des rongeurs, il évolue vers les silhouettes de personnages, puis lassé des interventions policières, il décide de ne peindre que sur des murs légaux. Ses œuvres représentent souvent des figures d’anonymes ou de célébrités en taille réelle Ses peintures représentent généralement des figures anonymes ou célèbres, de taille réelle. Il puise souvent son inspiration dans des personnages du quotidien, issus de la réalité urbaine, dont il reproduit ensuite l\'image dans le décor où il évolue.\r\n');
 
 -- --------------------------------------------------------
 
@@ -84,7 +84,7 @@ CREATE TABLE IF NOT EXISTS `oeuvre` (
   `artiste_id` tinyint(2) UNSIGNED DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `foreign_key` (`artiste_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `oeuvre`
@@ -112,7 +112,7 @@ INSERT INTO `oeuvre` (`id`, `titre`, `photo`, `localisation`, `artiste_id`) VALU
 (19, 'Bird girl', 'cbirdgirl.jpg', 'Paris 13e / France', 3),
 (20, 'Dali', 'cdali.jpg', 'Madrid / Espagne', 3),
 (21, 'Le chat', 'clechat.jpg', 'Djerba / Tunisie', 3),
-(22, 'Lincoln', 'clincoln.jpg', 'Vincennes / France', 3),
+(22, 'Catwoman', 'ccatwoman.jpg', 'Vincennes / France', 3),
 (23, 'Moon girl', 'cmoongirl.jpg', 'Londres / Angleterre', 3),
 (24, 'Vieil homme', 'cvieilhomme.png', 'Vitry / France', 3),
 (25, 'Abus', 'mabus.jpg', 'Paris / France', 4),
@@ -120,7 +120,8 @@ INSERT INTO `oeuvre` (`id`, `titre`, `photo`, `localisation`, `artiste_id`) VALU
 (27, 'Ca va passer', 'mcavapasser.jpg', 'Paris / France', 4),
 (28, 'Illusion', 'millusion.jpg', 'Paris 13e / France', 4),
 (29, 'Le Vain', 'mlevain.jpg', 'Paris / France', 4),
-(30, 'Simple', 'msimple.jpg', 'Paris / France', 4);
+(30, 'Simple', 'msimple.jpg', 'Paris / France', 4),
+(31, 'ggg', 'Banksy.on.the.thekla.arp.jpg', 'lll', NULL);
 
 --
 -- Contraintes pour les tables déchargées
